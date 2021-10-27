32602050ecb332a4e3c391675175dcea37337a5c
unknown <rjmahir.faisal@gmail.com>
2021-10-27 12:19:35 +0600

    name pass deleted

--- index.js
+++ index.js
@@ -15,9 +15,6 @@ app.use(express.json());
 
 
 
-// Name: genius-car-service //
-// pass: LPhsNrde14JSHt5D //
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.uwoya.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
